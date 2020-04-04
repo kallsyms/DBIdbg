b1da6647e1b892a11001943eb21e61a35e187f34
Nick Gregory <computerfreak97@gmail.com>
2020-04-04 03:11:27 -0400

    trampolining

--- src/main.rs
+++ src/main.rs
@@ -75,6 +75,10 @@ struct TranslatorState<'a> {
 }
 
 impl<'a> TranslatorState<'a> {
+    fn side_stack_rsp_loc(&self) -> u64 {
+        return (self.side_stack.data() as u64) + self.side_stack.len() as u64 - 8;
+    }
+
     fn new(contents: &'a Vec<u8>) -> Result<TranslatorState<'a>, &'static str> {
         let binary = match xmas_elf::ElfFile::new(contents) {
             Ok(b) => b,
@@ -92,12 +96,13 @@ impl<'a> TranslatorState<'a> {
             None => return Err("Failed to look up .text section"),
         };
 
-        let side_stack = mmap::MemoryMap::new(0x10000, &[
+        let side_stack = mmap::MemoryMap::new(0x1000, &[
             mmap::MapOption::MapReadable,
             mmap::MapOption::MapWritable,
+            mmap::MapOption::MapAddr(0x100_000 as *const u8), // pin this to a 32-bit mem addr so it can be xchg'd
         ]).unwrap();
 
-        return Ok(TranslatorState {
+        let ts = TranslatorState{
             binary,
             bitness,
             text_section,
@@ -105,7 +110,17 @@ impl<'a> TranslatorState<'a> {
             mapped_regions: Vec::new(),
             allocator: CodeRegionAllocator::new(),
             decoded_bbs: HashMap::new(),
-        });
+        };
+
+        unsafe {
+            // Write the address of the end of the stack region itself to where we're going to be
+            // swapping RSP from/to
+            (ts.side_stack_rsp_loc() as *mut u64).write(
+                (ts.side_stack.data() as u64) + ts.side_stack.len() as u64 - 0x10
+            );
+        }
+
+        return Ok(ts);
     }
 
     fn map_file(&mut self) -> Result<(), mmap::MapError> {
@@ -191,11 +206,114 @@ impl<'a> TranslatorState<'a> {
         return self.decoded_bbs.get(&split_address).unwrap();
     }
 
-    fn rewrite_instr(&mut self, instr: Instruction) -> Vec<Instruction> {
+    fn generate_trampoline(&self, pc: u64, cb: fn(&mut [u64; 16])) -> Vec<Instruction> {
+        let save_regs = [
+            iced_x86::Register::RAX,
+            iced_x86::Register::RBX,
+            iced_x86::Register::RCX,
+            iced_x86::Register::RDX,
+            iced_x86::Register::RDI,
+            iced_x86::Register::RSI,
+            iced_x86::Register::RBP,
+            iced_x86::Register::R8,
+            iced_x86::Register::R9,
+            iced_x86::Register::R10,
+            iced_x86::Register::R11,
+            iced_x86::Register::R12,
+            iced_x86::Register::R13,
+            iced_x86::Register::R14,
+            iced_x86::Register::R15,
+        ];
+
+        let stack_swap = Instruction::with_mem_reg(
+            iced_x86::Code::Xchg_rm64_r64,
+            iced_x86::MemoryOperand::new(
+                iced_x86::Register::None,
+                iced_x86::Register::None,
+                1,
+                self.side_stack_rsp_loc() as i32,
+                8,
+                false,
+                iced_x86::Register::None,
+            ),
+            iced_x86::Register::RSP,
+        );
+
+        let mut insns = vec![
+            stack_swap,
+        ];
+
+        for reg in save_regs.iter() {
+            insns.push(Instruction::with_reg(
+                iced_x86::Code::Push_r64,
+                *reg,
+            ));
+        }
+
+        // push guest PC
+        insns.extend(vec![
+            Instruction::with_reg_u64(
+                iced_x86::Code::Mov_r64_imm64,
+                iced_x86::Register::RAX,
+                pc,
+            ),
+            Instruction::with_reg(
+                iced_x86::Code::Push_r64,
+                iced_x86::Register::RAX,
+            ),
+        ]);
+
+        insns.extend(vec![
+            Instruction::with_reg_reg(
+                iced_x86::Code::Mov_rm64_r64,
+                iced_x86::Register::RDI,
+                iced_x86::Register::RSP,
+            ),
+            Instruction::with_reg_u64(
+                iced_x86::Code::Mov_r64_imm64,
+                iced_x86::Register::RAX,
+                cb as u64,
+            ),
+            // align stack - rust cbs will need it it 16-byte aligned for xmm stuff
+            Instruction::with_reg_i32(
+                iced_x86::Code::And_rm64_imm8,
+                iced_x86::Register::RSP,
+                -0x10,
+            ),
+            Instruction::with_reg(
+                iced_x86::Code::Call_rm64,
+                iced_x86::Register::RAX,
+            ),
+        ]);
+
+        // add back over pc
+        insns.extend(vec![
+            Instruction::with_reg_i32(
+                iced_x86::Code::Add_rm64_imm8,
+                iced_x86::Register::RSP,
+                8,
+            ),
+        ]);
+
+        for reg in save_regs.iter().rev() {
+            insns.push(Instruction::with_reg(
+                iced_x86::Code::Pop_r64,
+                *reg,
+            ));
+        }
+
+        insns.push(stack_swap);
+
+        return insns;
+    }
+
+    fn rewrite_instr(&mut self, instr: Instruction) -> (Vec<Instruction>, bool) {
         let mut mem_access_ops: Vec<u32> = Vec::with_capacity(5);
 
         for op_idx in 0..instr.op_count() {
-            if instr.op_kind(op_idx) == OpKind::Memory {
+            if instr.op_kind(op_idx) == OpKind::Memory && (
+                    instr.memory_base() != iced_x86::Register::None || 
+                    instr.memory_index() != iced_x86::Register::None) {
                 mem_access_ops.push(op_idx);
             }
         }
@@ -206,24 +324,38 @@ impl<'a> TranslatorState<'a> {
             _ => false,
         };
 
-        if mem_access_ops.len() == 0 && is_non_branch_instr {
-            // instr can be directly lifted
-            println!("Lifting {} directly", instr);
-            return vec![instr];
-        }
-
         let mut instrs: Vec<Instruction> = Vec::new();
+
+        let mut info_factory = iced_x86::InstructionInfoFactory::new();
+        let info = info_factory.info(&instr);
 
         for mem_op_idx in mem_access_ops.iter() {
             // rewrite the mem load(s)/store(s)
             println!("Rewriting mem load/store in {} op {}", instr, mem_op_idx);
-            // TODO
-            instrs.push(instr);
+            match info.op_access(*mem_op_idx) {
+                iced_x86::OpAccess::Write => (),  // TODO: check for SMC
+                _ => (),
+            }
+            instrs.extend(self.generate_trampoline(instr.ip(), |regs: &mut [u64; 16]| {
+                println!("regs: {:?}", regs);
+            }));
         }
 
-        let (needs_translate_stub, branch_type) = match (instr.code(), instr.flow_control()) {
-            (_, FlowControl::UnconditionalBranch) => (false, iced_x86::Code::Jmp_rel32_64),
-            (_, FlowControl::Call) => (false, iced_x86::Code::Call_rel32_64),
+        instrs.push(instr);
+
+        if is_non_branch_instr {
+            if mem_access_ops.len() == 0 {
+                // instr can be directly lifted
+                println!("Lifting {} directly", instr);
+            }
+
+            return (instrs, true);
+        }
+
+        let (needs_translate_stub, branch_type, decode_next) = match (instr.code(), instr.flow_control()) {
+            (_, FlowControl::UnconditionalBranch) => (false, iced_x86::Code::Jmp_rel32_64, false),
+            (_, FlowControl::ConditionalBranch) => (false, instr.code(), true),
+            (_, FlowControl::Call) => (false, iced_x86::Code::Call_rel32_64, false),
             _ => {
                 panic!("Unhandled control flow change instruction {:?}", instr.flow_control());
             }
@@ -231,6 +363,7 @@ impl<'a> TranslatorState<'a> {
 
         let guest_target = instr.near_branch_target();
         if let Some(bb) = self.decoded_bbs.get(&guest_target) {
+            println!("Target of {} ({:#x}) already decoded, adding trampoline", instr, guest_target);
             instrs.push(Instruction::with_branch(branch_type, bb.jit_address));
         } else if self.is_decoded(guest_target) {
             // jump into existing BB, but not the beginning.
@@ -239,14 +372,17 @@ impl<'a> TranslatorState<'a> {
             instrs.push(Instruction::with_branch(branch_type, new_bb.jit_address));
         } else {
             // new code, preallocate a target code region and create the jump to it
+            println!("Decoding target of {} ({:#x})", instr, guest_target);
             let target_bb = self.decode_at(guest_target);
             instrs.push(Instruction::with_branch(branch_type, target_bb.jit_address));
         }
 
-        return instrs;
+        return (instrs, decode_next);
     }
 
     fn decode_at(&mut self, address: u64) -> &BasicBlock {
+        println!("Decoding at {:#x}", address);
+
         let text: &[u8] = unsafe { std::slice::from_raw_parts(self.text_section.address() as *const u8, self.text_section.size() as usize) };
         let mut decoder = Decoder::new(self.bitness, text, DecoderOptions::NONE);
         decoder.set_position((address - self.text_section.address()) as usize);
@@ -267,11 +403,11 @@ impl<'a> TranslatorState<'a> {
                 break;
             }
 
-            let rewritten_instrs = self.rewrite_instr(instr);
+            let (rewritten_instrs, decode_next) = self.rewrite_instr(instr);
 
             bb_instrs.extend(rewritten_instrs);
 
-            if instr.flow_control() != FlowControl::Next && instr.code() != iced_x86::Code::Syscall {
+            if !decode_next {
                 // This is the end of the BB
                 break;
             }
@@ -296,8 +432,13 @@ impl<'a> TranslatorState<'a> {
         return self.decoded_bbs.entry(address).or_insert(bb);
     }
 
-    fn decode_start(&mut self) -> &BasicBlock {
-        return self.decode_at(self.binary.header.pt2.entry_point());
+    fn decode(&mut self) {
+        self.decode_at(self.binary.header.pt2.entry_point());
+    }
+
+    fn run(&self) {
+        let entry_bb = self.decoded_bbs.get(&self.binary.header.pt2.entry_point()).unwrap();
+        entry_bb.call();
     }
 }
 
@@ -321,6 +462,21 @@ fn main() {
         panic!(map_result.err().unwrap());
     }
 
-    let start_bb = translator.decode_start();
-    start_bb.call();
+    println!("Decoding from entrypoint");
+    translator.decode();
+
+    // debug output all of our rewritten blocks
+    println!("Rewritten entrypoint:");
+    for (_, bb) in translator.decoded_bbs.iter() {
+        let code: &[u8] = unsafe { std::slice::from_raw_parts(bb.jit_address as *const u8, bb.size) };
+        let mut decoder = Decoder::new(translator.bitness, code, DecoderOptions::NONE);
+        decoder.set_ip(bb.jit_address);
+
+        while decoder.can_decode() {
+            let instr = decoder.decode();
+            println!("{}", instr);
+        }
+    }
+
+    translator.run();
 }
